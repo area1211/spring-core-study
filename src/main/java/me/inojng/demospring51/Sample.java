13b98c30f08a9760da32942001bec046cf005ef9
Jung Inho <area1211@naver.com>
2019-05-05 01:33:37 +0900

    17. SpEL

---
+++ src/main/java/me/inojng/demospring51/Sample.java
@@ -0,0 +1,17 @@
+package me.inojng.demospring51;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Sample {
+
+    private int data = 200;
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+}
