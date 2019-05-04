edb901016b29af2aee2fbb75068e6775ba8816eb
Jung Inho <area1211@naver.com>
2019-05-04 23:20:39 +0900

    IoC 컨테이너 9부: ResourceLoader

--- src/main/java/me/inojng/demospring51/AppRunner.java
+++ src/main/java/me/inojng/demospring51/AppRunner.java
@@ -3,18 +3,23 @@ package me.inojng.demospring51;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ApplicationEventPublisher applicationContext;
+    ResourceLoader resourceLoader;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        applicationContext.publishEvent(new MyEvent(this, 100));
+        Resource resource = resourceLoader.getResource("classpath:test.txt");
+        System.out.println(resource.exists());
+        System.out.println(resource.getDescription());
     }
 }
