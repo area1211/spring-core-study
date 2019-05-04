762f5fbed240d835b11905008540963f5efdddf8
Jung Inho <area1211@naver.com>
2019-05-05 00:02:24 +0900

    Validation 추상화

--- src/main/java/me/inojng/demospring51/AppRunner.java
+++ src/main/java/me/inojng/demospring51/AppRunner.java
@@ -8,26 +8,41 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ApplicationContext resourceLoader;
+    Validator validator;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(resourceLoader.getClass());
+        System.out.println(validator.getClass());
 
+        Event event = new Event();
+        event.setLimit(-1);
+        event.setEmail("asdf");
+//        EventValidator eventValidator = new EventValidator();
+        // BeanPropertyBindingResult 클래스는
+        // 스프링 MVC에서 자동으로 생성해서 파라미터에 전달해주기 때문에 직접 사용할 일은 없다.
+        Errors errors = new BeanPropertyBindingResult(event, "event");
 
-        Resource resource = resourceLoader.getResource("test.txt");
-        System.out.println(resource.getClass());
+//        eventValidator.validate(event, errors);
+        validator.validate(event, errors);
 
-        System.out.println(resource.exists());
-        System.out.println(resource.getDescription());
+        System.out.println(errors.hasErrors());
 
+        errors.getAllErrors().forEach(e -> {
+            System.out.println("======== error code ========");
+            Arrays.stream(e.getCodes()).forEach(System.out::println);
+            System.out.println(e.getDefaultMessage());
+        });
     }
 }
