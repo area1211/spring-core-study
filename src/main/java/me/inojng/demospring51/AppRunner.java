339f5bc54d1c31b4213ee0d4f9c44e400159af38
Jung Inho <area1211@naver.com>
2019-05-04 23:38:09 +0900

    Resource 추상화

--- src/main/java/me/inojng/demospring51/AppRunner.java
+++ src/main/java/me/inojng/demospring51/AppRunner.java
@@ -3,6 +3,8 @@ package me.inojng.demospring51;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
@@ -14,12 +16,18 @@ import java.nio.file.Path;
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ResourceLoader resourceLoader;
+    ApplicationContext resourceLoader;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Resource resource = resourceLoader.getResource("classpath:test.txt");
+        System.out.println(resourceLoader.getClass());
+
+
+        Resource resource = resourceLoader.getResource("test.txt");
+        System.out.println(resource.getClass());
+
         System.out.println(resource.exists());
         System.out.println(resource.getDescription());
+
     }
 }
